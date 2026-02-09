2efbfc367c28d5f28e4f4dc3cb7c5ce1b8efa616
KBDub <128649700+KBDub@users.noreply.github.com>
2026-02-09 05:26:14 +0000

    upped

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,6 @@
 
     <main>
         <x-sections.hero />
-        <x-sections.top5pct-info />
         <x-sections.our-services />
         <x-sections.cta-design-your-own />
         <x-sections.why-choose-us />
